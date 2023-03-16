f98eab589facc663073b4aac276eca289c1002b4
Lobo93 <pablo.lobo@sensedia.com>
2023-03-16 20:12:43 -0300

    New features
    - Added colorful ants
    - Added new board patterns

--- main.js
+++ main.js
@@ -2,8 +2,9 @@
 const boardWidth = 200
 const boardHeight = 200
 const squareSize = 4
-const color0 = 'rgba(250,250,250,255)'
-const color1 = 'rgba(5,5,5,255)'
+const color0 = '#eee'
+let color1 = '#111'
+let boardType = 'clean'
 let interval = 100
 let playing = 1
 
@@ -24,7 +25,8 @@ context.fillRect(0, 0, boardWidth * squareSize, boardHeight * squareSize)
 // Ants
 const ants = []
 
-function Ant(direction, x, y) {
+function Ant(color, direction, x, y) {
+	this.color = color
 	this.direction = direction
 	this.x = x
 	this.y = y
@@ -33,6 +35,7 @@ function Ant(direction, x, y) {
 // Add new ants
 canvas.addEventListener('click', ({offsetX, offsetY}) => {
 	ants.push(new Ant(
+		color1,
 		'up',
 		Math.floor(offsetX * canvas.width / canvas.clientWidth / squareSize),
 		Math.floor(offsetY * canvas.height / canvas.clientHeight / squareSize)
@@ -65,7 +68,7 @@ function cycle() {
 	
 			// Change square color
 			board[ant.y][ant.x] ^= 1
-			context.fillStyle = board[ant.y][ant.x] === 0 ? color0 : color1
+			context.fillStyle = board[ant.y][ant.x] === 0 ? color0 : ant.color
 			context.fillRect(ant.x * squareSize, ant.y * squareSize, squareSize, squareSize)
 	
 			// Move forward 1 square
@@ -104,8 +107,43 @@ play.addEventListener('click', () => {
 	playing ^= 1
 })
 
+const download = document.getElementById('download')
+download.addEventListener('click', () => {
+	download.href = canvas.toDataURL()
+})
+
+const menu = document.getElementById('menu')
+
+const menuButton = document.getElementById('menuButton')
+menuButton.addEventListener('click', () => {
+	menuButton.classList.toggle('icon-bars')
+	menuButton.classList.toggle('icon-close')
+	menu.classList.toggle('active')
+})
+
+const colorButtons = document.querySelectorAll('.color-button')
+colorButtons.forEach(button => {
+	button.addEventListener('click', ({target}) => {
+		colorButtons.forEach(otherButtons => otherButtons.classList.remove('active'))
+		target.classList.add('active')
+		color1 = target.dataset.color
+	})
+})
+
+const boardButtons = document.querySelectorAll('.board-button')
+boardButtons.forEach(button => {
+	button.addEventListener('click', ({target}) => {
+		boardButtons.forEach(otherButtons => otherButtons.classList.remove('active'))
+		target.classList.add('active')
+		boardType = target.dataset.board
+		newBoard()
+	})
+})
+
 const reset = document.getElementById('reset')
-reset.addEventListener('click', () => {
+reset.addEventListener('click', newBoard)
+
+function newBoard() {
 	board.forEach((row, index) => board[index] = Array(boardWidth).fill(0))
 	ants.length = 0
 	context.fillStyle = color0
@@ -113,9 +151,48 @@ reset.addEventListener('click', () => {
 	play.classList.remove('icon-play')
 	play.classList.add('icon-pause')
 	playing = 1
-})
-
-const download = document.getElementById('download')
-download.addEventListener('click', () => {
-	download.href = canvas.toDataURL()
-})
+	if (boardType === 'dots') {
+		for (let x = 0; x < boardWidth; x++) {
+			for (let y = 0; y < boardHeight; y++) {
+				if (x % 2 != 0 && y % 2 != 0) {
+					board[y][x] = 1
+					context.fillStyle = '#111'
+					context.fillRect(x * squareSize, y * squareSize, squareSize, squareSize)
+				}
+			}
+		}
+	}
+	else if (boardType === 'lines') {
+		for (let x = 0; x < boardWidth; x++) {
+			for (let y = 0; y < boardHeight; y++) {
+				if ((x - y) % 8 === 0) {
+					board[y][x] = 1
+					context.fillStyle = '#111'
+					context.fillRect(x * squareSize, y * squareSize, squareSize, squareSize)
+				}
+			}
+		}
+	}
+	else if (boardType === 'box') {
+		for (let x = 0; x < boardWidth; x++) {
+			for (let y = 0; y < boardHeight; y++) {
+				if (
+					y >= Math.floor(boardHeight * 0.2) &&
+					y <= Math.floor(boardHeight * 0.8) && (
+						x === Math.floor(boardWidth * 0.2) ||
+						x === Math.floor(boardWidth * 0.8)
+					) ||
+					x >= Math.floor(boardWidth * 0.2) &&
+					x <= Math.floor(boardWidth * 0.8) && (
+						y === Math.floor(boardHeight * 0.2) ||
+						y === Math.floor(boardHeight * 0.8)
+					)
+				) {
+					board[y][x] = 1
+					context.fillStyle = '#111'
+					context.fillRect(x * squareSize, y * squareSize, squareSize, squareSize)
+				}
+			}
+		}
+	}
+}
